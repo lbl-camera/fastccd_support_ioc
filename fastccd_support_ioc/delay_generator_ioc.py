b963b6aa00539f9f88aad270c8385f6945c49d07
Juliane Reinhardt <jreinhardt@lbl.gov>
2020-08-05 10:36:18 -0700

    modify some PV's --> working state

--- fastccd_support_ioc/delay_generator_ioc.py
+++ fastccd_support_ioc/delay_generator_ioc.py
@@ -9,8 +9,12 @@ def ibterm(command, caster=None):
     command = f'/bin/bash -c "ibterm -d 15 <<< \\\"{command}\\\""'
     stdout = subprocess.check_output(command, shell=True)
     if caster:
-        return caster(stdout.decode().split("\n")[2].strip("ibterm>"))
-
+        try:
+            return caster(stdout.decode().split("\n")[2].strip("ibterm>"))
+        except ValueError:
+            return caster(stdout.decode().split("\n")[2].strip("ibterm>").split(",")[-1])
+        # except ValueError:
+        #     return caster(stdout.decode().split("\n")[2].strip("ibterm>2,"))
 
 pvproperty_with_rbv = get_pv_pair_wrapper(setpoint_suffix='',
                                           readback_suffix='_RBV')
@@ -23,11 +27,18 @@ class DelayGenerator(PVGroup):
 
     trigger_rate = pvproperty_with_rbv(dtype=float, doc="TriggerRate")
     trigger_mode = pvproperty_with_rbv(dtype=int, doc="TriggerMode")
-    delay_time = pvproperty_with_rbv(type=float, doc="DelayTime")
-    shutter_time =  pvproperty_with_rbv(type=float, doc="ShutterTime")
+    delay_time = pvproperty_with_rbv(dtype=float, doc="DelayTime")
+    shutter_time =  pvproperty_with_rbv(dtype=float, doc="ShutterTime")
+    output_offset = pvproperty_with_rbv(dtype=int, doc="OutputOffset")
 
-    # trigger_mode = pvproperty_with_rbv("TriggerMode")
-    # trigger_enabled = pvproperty_with_rbv("TriggerEnabled")
+    @output_offset.setpoint.putter
+    async def output_offset(obj, instance, offset):
+        ibterm(f"oo 1,{offset}")
+        # await obj.readback.write(value)
+
+    @output_offset.readback.getter
+    async def output_offset(obj, instance):
+        return ibterm(f"oo 1", int)
 
     @trigger_rate.setpoint.putter
     async def trigger_rate(obj, instance, rate):
@@ -39,30 +50,31 @@ class DelayGenerator(PVGroup):
         return ibterm(f"tr 0", float)
 
     @trigger_mode.setpoint.putter
-    async def trigger_mode(obj, instance, mode):
-        "TriggerMode = 0 is int, TriggerMode 2 is SS = Single-Shot trigger"
-        ibterm(f"tm ,{mode}")
+    async def trigger_mode(obj, instance, rate):
+        ibterm(f"tm {rate}")
+        # await obj.readback.write(value)
 
-    @trigger_mode.setpoint.getter
+    @trigger_mode.readback.getter
     async def trigger_mode(obj, instance):
-        ibterm(f"tm")
+        return ibterm(f"tm", int)
 
     @delay_time.setpoint.putter
-    async def delay_time(obj, instance, sec ):
-        ibterm(f"dt 2,1,{sec}")
+    async def delay_time(obj, instance, rate):
+        ibterm(f"dt 2,1,{rate}")
+        # await obj.readback.write(value)
 
-    @delay_time.setpoint.getter
+    @delay_time.readback.getter
     async def delay_time(obj, instance):
-        ibterm(f"dt 2")
+        return ibterm(f"dt 2", float)
 
     @shutter_time.setpoint.putter
-    async def shutter_time(obj, instance, sec ):
-        ibterm(f"dt 3,2,{sec}")
+    async def shutter_time(obj, instance, rate):
+        ibterm(f"dt 3,2,{rate}")
+        # await obj.readback.write(value)
 
-    @shutter_time.setpoint.getter
+    @shutter_time.readback.getter
     async def shutter_time(obj, instance):
-        ibterm(f"dt 2")
-
+        return ibterm(f"dt 3", float)
 
 def main():
     """Console script for fastccd_support_ioc."""
