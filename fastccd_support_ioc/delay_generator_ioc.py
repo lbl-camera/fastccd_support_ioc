754f4ea7fa08c9fe36158c0c707e15aec99df7b9
ronpandolfi <ronpandolfi@gmail.com>
2020-08-06 17:43:00 -0700

    Refactor setup into enum'd pvproperty with startup/shutdown; add logging; add todos; use bool casting for booleans; better default prefix; simpler parsing logic
    (needs testing on COSMIC)

--- fastccd_support_ioc/delay_generator_ioc.py
+++ fastccd_support_ioc/delay_generator_ioc.py
@@ -3,16 +3,24 @@ import subprocess
 from textwrap import dedent
 import sys
 from caproto.server import ioc_arg_parser, run
+from caproto import ChannelType
+import logging
+
+logger = logging.getLogger('caproto')
+
+
+# TODO: rename PVs to compliant convention
+# TODO: decide on PV naming convention
+# TODO: add docs to PVs
+# TODO: Should ibterm only ever return one value, or would we ever want multiple values back from a command?
+
 
 def ibterm(command, caster=None):
     command = f'/bin/bash -c "ibterm -d 15 <<< \\\"{command}\\\""'
-    print(command)
+    logger.debug('exec:', command)
     stdout = subprocess.check_output(command, shell=True)
     if caster:
-        try:
-            return caster(stdout.decode().split("\n")[2].strip("ibterm>"))
-        except ValueError:
-            return caster(stdout.decode().split("\n")[2].strip("ibterm>").split(",")[-1])
+        return caster(stdout.decode().split("\n")[2].strip("ibterm>").split(",")[-1])
 
 
 pvproperty_with_rbv = get_pv_pair_wrapper(setpoint_suffix='',
@@ -25,72 +33,98 @@ class DelayGenerator(PVGroup):
     """
 
     trigger_rate = pvproperty_with_rbv(dtype=float, doc="TriggerRate")
-    trigger_on_off = pvproperty_with_rbv(dtype=float, doc="TriggerOnOFF")
+    trigger_on_off = pvproperty_with_rbv(dtype=bool, doc="TriggerOnOFF")
     delay_time = pvproperty_with_rbv(dtype=float, doc="DelayTime")
-    shutter_time =  pvproperty_with_rbv(dtype=float, doc="ShutterTime")
-
-    #additional PV only used for setup
-    setup = pvproperty_with_rbv(dtype=float, doc="Setup")
-
-
-    @setup.setpoint.putter
-    async def setup(obj, instance, init):
-        if init == 1:
-            # clear and setup various parameters
-            ibterm(f"CL; DT 2,1,1E-3; DT 3,2,140E-3; TZ 1,1; TZ 4,1; OM 4,0; OM 1,3; OA 1,3.3; OO 1,0; TR 0,5")
-        elif init == 0:
-            # only clear device
-            ibterm(f"CL")
+    shutter_time = pvproperty_with_rbv(dtype=float, doc="ShutterTime")
 
     @trigger_rate.setpoint.putter
     async def trigger_rate(obj, instance, value):
         ibterm(f"tr 0,{value}")
-        # await obj.readback.write(value)
 
     @trigger_rate.readback.getter
     async def trigger_rate(obj, instance):
         return ibterm(f"tr 0", float)
 
     @trigger_on_off.setpoint.putter
-    async def trigger_on_off(obj, instance, boolean):
-        if boolean == 0:
-            ibterm(f"tm 2")
-        elif boolean == 1:
+    async def trigger_on_off(obj, instance, on):
+        logger.debug('setting triggering:', on)
+        if on:
             ibterm(f"tm 0")
-         # await obj.readback.write(value)
-    
+        else:
+            ibterm(f"tm 2")
+
     @trigger_on_off.readback.getter
     async def trigger_on_off(obj, instance):
-        return ibterm(f"tm", int)
-    
+        return ibterm(f"tm", bool)
+
     @delay_time.setpoint.putter
-    async def delay_time(obj, instance, rate):
-        ibterm(f"dt 2,1,{rate}")
-        # await obj.readback.write(value)
-    
+    async def delay_time(obj, instance, delay):
+        ibterm(f"dt 2,1,{delay}")
+
     @delay_time.readback.getter
     async def delay_time(obj, instance):
         return ibterm(f"dt 2", float)
-    
+
     @shutter_time.setpoint.putter
-    async def shutter_time(obj, instance, rate):
-        ibterm(f"dt 3,2,{rate}")
-         # await obj.readback.write(value)
-    
+    async def shutter_time(obj, instance, shutter_time):
+        ibterm(f"dt 3,2,{shutter_time}")
+
     @shutter_time.readback.getter
     async def shutter_time(obj, instance):
         return ibterm(f"dt 3", float)
+
+    state = pvproperty(dtype=ChannelType.ENUM, enum_strings=["unknown", "initialized", "off", ])
+
+    async def initialize(self, instance, value):
+        # clear and setup various parameters
+        ibterm(f"CL; DT 2,1,1E-3; DT 3,2,140E-3; TZ 1,1; TZ 4,1; OM 4,0; OM 1,3; OA 1,3.3; OO 1,0; TR 0,5")
+
+    async def shutdown(self, instance, value):
+        # only clear device
+        ibterm(f"CL")
+
+    @state.getter
+    async def state(self, instance):
+        return instance.value
+
+    @state.putter
+    async def state(self, instance, value):
+        if value != instance.value:
+            logger.debug("setting state:", value)
+
+            if value == "initialized":
+                await self.initialize(None, None)
+
+            elif value == "off":
+                await self.shutdown(None, None)
+
+        return value
+
+    initialize = pvproperty(value=0, dtype=int, put=initialize)
+    shutdown = pvproperty(value=0, dtype=int, put=shutdown)
+
+    @state.startup
+    async def state(self, instance, async_lib):
+        await self.initialize(None, None)
+
+    @state.shutdown
+    async def state(self, instance, async_lib):
+        await self.initialize(None, None)
 
 
 def main():
     """Console script for fastccd_support_ioc."""
 
     ioc_options, run_options = ioc_arg_parser(
-        default_prefix='XF:7011:',
+        default_prefix='XF:7011:ShutterDelayGenerator:',
         desc=dedent(DelayGenerator.__doc__))
     ioc = DelayGenerator(**ioc_options)
 
-    #print("#" * 80, conversion.group_to_device(ioc), "#" * 80, sep='\n')
+    logger.info("\nAuto-generated Ophyd device for this PVGroup",
+                "#" * 80,
+                conversion.group_to_device(ioc),
+                "#" * 80,
+                sep='\n')
 
     run(ioc.pvdb, **run_options)
 
