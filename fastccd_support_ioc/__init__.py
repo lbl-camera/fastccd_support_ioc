7a8b230d0e61e019a7a44e4962896beba235935d
Ronald Pandolfi <ronpandolfi@gmail.com>
2021-02-17 17:53:29 -0800

    Reduce autosave period to 1 sec; prune imports

--- fastccd_support_ioc/__init__.py
+++ fastccd_support_ioc/__init__.py
@@ -6,14 +6,18 @@ __version__ = '0.1.0'
 
 from . import utils
 from caproto.server import PVGroup, get_pv_pair_wrapper
-from caproto.server.autosave import autosaved
+from caproto.server.autosave import autosaved, AutosaveHelper
 
 
 pvproperty_with_rbv = get_pv_pair_wrapper(setpoint_suffix='',
                                           readback_suffix='_RBV')
 
 
-def wrap_autosave(pvgroup:PVGroup):
+class FastAutosaveHelper(AutosaveHelper):
+    period = 1
+
+
+def wrap_autosave(pvgroup: PVGroup):
     pvgroup.readback = autosaved(pvgroup.readback)
     pvgroup.setpoint = autosaved(pvgroup.setpoint)
     return pvgroup
