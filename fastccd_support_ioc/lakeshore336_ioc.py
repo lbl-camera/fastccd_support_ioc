ed061b72d9e703b7ba714c0958dc60a3238a9f36
Ronald Pandolfi <ronpandolfi@gmail.com>
2021-10-01 16:24:12 -0700

    Add second lakeshore channel

--- fastccd_support_ioc/lakeshore336_ioc.py
+++ fastccd_support_ioc/lakeshore336_ioc.py
@@ -18,40 +18,71 @@ class LakeshoreModel336(PVGroup):
     https://www.lakeshore.com/docs/default-source/product-downloads/336_manual.pdf?sfvrsn=fa4e3a80_5
     """
 
-    TemperatureCelsius = pvproperty(dtype=float, doc="Temperature in Celsius", precision=2)
-    TemperatureKelvin = pvproperty(dtype=float, doc="Temperature in Kelvin", precision=2)
+    TemperatureCelsiusA = pvproperty(dtype=float, doc="Temperature in Celsius", precision=2)
+    TemperatureKelvinA = pvproperty(dtype=float, doc="Temperature in Kelvin", precision=2)
+    TemperatureCelsiusB = pvproperty(dtype=float, doc="Temperature in Celsius", precision=2)
+    TemperatureKelvinB = pvproperty(dtype=float, doc="Temperature in Kelvin", precision=2)
     HeaterOutput = pvproperty(dtype=float, doc="Heater output in %", precision=2)
 
-    TemperatureLimit = pvproperty_with_rbv(dtype=float, doc="Temperature Limit (input A) in Kelvin for which to shut down"
-                                                     "all control outputs when exceeded. A temperature limit of "
-                                                     "zero turns the Temperature limit feature off for the given "
-                                                            "sensor input.", precision=2)
+    TemperatureLimitA = pvproperty_with_rbv(dtype=float,
+                                            doc="Temperature Limit (input A) in Kelvin for which to shut down"
+                                                "all control outputs when exceeded. A temperature limit of "
+                                                "zero turns the Temperature limit feature off for the given "
+                                                "sensor input.", precision=2)
+    TemperatureLimitB = pvproperty_with_rbv(dtype=float,
+                                            doc="Temperature Limit (input A) in Kelvin for which to shut down"
+                                                "all control outputs when exceeded. A temperature limit of "
+                                                "zero turns the Temperature limit feature off for the given "
+                                                "sensor input.", precision=2)
     TemperatureSetPoint = pvproperty_with_rbv(dtype=float, doc="Temperature set point", value=-20.0, precision=2)
 
-    @TemperatureCelsius.getter
-    async def TemperatureCelsius(obj, instance):
+    @TemperatureCelsiusA.getter
+    async def TemperatureCelsiusA(obj, instance):
         return float(lakeshore336.query('CRDG?'))
 
-    @TemperatureCelsius.scan(period=1)
-    async def TemperatureCelsius(obj, instance, async_lib):
+    @TemperatureCelsiusA.scan(period=1)
+    async def TemperatureCelsiusA(obj, instance, async_lib):
         await instance.write(float(lakeshore336.query('CRDG?')))
 
-    @TemperatureKelvin.getter
-    async def TemperatureKelvin(obj, instance):
+    @TemperatureKelvinA.getter
+    async def TemperatureKelvinA(obj, instance):
         return float(lakeshore336.query('KRDG?'))
 
-    @TemperatureKelvin.scan(period=1)
-    async def TemperatureKelvin(obj, instance, async_lib):
+    @TemperatureKelvinA.scan(period=1)
+    async def TemperatureKelvinA(obj, instance, async_lib):
         await instance.write(float(lakeshore336.query('KRDG?')))
 
-    #TODO check which input channel is required?
-    @TemperatureLimit.readback.getter
-    async def TemperatureLimit(obj, instance):
+    @TemperatureLimitA.readback.getter
+    async def TemperatureLimitA(obj, instance):
         return float(lakeshore336.query('TLIMIT? A'))
 
-    @TemperatureLimit.setpoint.putter
-    async def TemperatureLimit(obj, instance, value):
+    @TemperatureLimitA.setpoint.putter
+    async def TemperatureLimitA(obj, instance, value):
         lakeshore336.query(f'TLIMIT A, {value}')
+
+    @TemperatureCelsiusB.getter
+    async def TemperatureCelsiusB(obj, instance):
+        return float(lakeshore336.query('CRDG? B'))
+
+    @TemperatureCelsiusB.scan(period=1)
+    async def TemperatureCelsiusB(obj, instance, async_lib):
+        await instance.write(float(lakeshore336.query('CRDG? B')))
+
+    @TemperatureKelvinB.getter
+    async def TemperatureKelvinB(obj, instance):
+        return float(lakeshore336.query('KRDG? B'))
+
+    @TemperatureKelvinB.scan(period=1)
+    async def TemperatureKelvinB(obj, instance, async_lib):
+        await instance.write(float(lakeshore336.query('KRDG? B')))
+
+    @TemperatureLimitB.readback.getter
+    async def TemperatureLimitB(obj, instance):
+        return float(lakeshore336.query('TLIMIT? B'))
+
+    @TemperatureLimitB.setpoint.putter
+    async def TemperatureLimitB(obj, instance, value):
+        lakeshore336.query(f'TLIMIT B, {value}')
 
     @HeaterOutput.getter
     async def HeaterOutput(obj, instance):
