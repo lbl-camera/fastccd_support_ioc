9fe72013e2bb1325305135fe8e35d96f99556fca
Ronald Pandolfi <ronpandolfi@gmail.com>
2022-11-10 20:03:26 -0800

    increase fops out1 current threshold

--- fastccd_support_ioc/utils/protection_checks.py
+++ fastccd_support_ioc/utils/protection_checks.py
@@ -35,7 +35,7 @@ def power_check_no_bias_clocks():
         assert _within_range(read_scalar('ES7011:FastCCD:BiasClocksPSU:Out2:Current'), .125, percent=.2)
         assert _within_range(read_scalar('ES7011:FastCCD:BiasClocksPSU:Out3:Current'), .03, percent=.2)
         assert _within_range(read_scalar('ES7011:FastCCD:BiasClocksPSU:Out4:Current'), .03, percent=.2)
-        assert _within_range(read_scalar('ES7011:FastCCD:FCRICFOPSPSU:Out1:Current'), 5, percent=.5)
+        assert _within_range(read_scalar('ES7011:FastCCD:FCRICFOPSPSU:Out1:Current'), 5, percent=.7)
         assert _within_range(read_scalar('ES7011:FastCCD:FCRICFOPSPSU:Out2:Current'), 2, percent=.5)
     except AssertionError:
         raise AssertionError('A PSU current is outside acceptable range!')
